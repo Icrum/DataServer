d38d32d16533a7a8521473c097e6d2deaa164fce
Icrum <hsals1234@gmail.com>
2021-01-11 16:19:57 +0000

    ProfileActivity is done.

--- app/src/main/java/com/example/dataserver/ProfileActivity.java
+++ app/src/main/java/com/example/dataserver/ProfileActivity.java
@@ -10,20 +10,27 @@ import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.EventListener;
 import com.google.firebase.firestore.FirebaseFirestore;
 import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class ProfileActivity extends AppCompatActivity {
@@ -31,7 +38,7 @@ public class ProfileActivity extends AppCompatActivity {
     FirebaseAuth fAuth;
     FirebaseFirestore fStore;
     String userID;
-    Button BtnSave, BtnImage;
+    Button BtnSave;
 
 
     @Override
@@ -62,6 +69,48 @@ public class ProfileActivity extends AppCompatActivity {
         toolbar.setTitle("Profile");
         setSupportActionBar(toolbar);
 
+        BtnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ProfileActivity.this.UpdateSettings();
+            }
+        });
+            
+
+    }
+
+    private void sendtoMain(){
+        startActivity(new Intent(getApplicationContext(),MainActivity.class));
+        finish();
+
+    }
+
+    private void UpdateSettings() {
+        String set_name = fullName.getText().toString();
+        String set_email= email.getText().toString();
+        fAuth=FirebaseAuth.getInstance();
+
+        if (TextUtils.isEmpty(set_name)){
+            Toast.makeText(this, "Please write your full name...", Toast.LENGTH_SHORT).show();
+        }
+        if (TextUtils.isEmpty(set_email)){
+            Toast.makeText(this, "Please write your email...", Toast.LENGTH_SHORT).show();
+        }
+        else{
+            userID=fAuth.getCurrentUser().getUid();
+            DocumentReference documentReference=fStore.collection("users").document(userID);
+            Map<String,Object> user=new HashMap<>();
+            user.put("email", email.getText().toString());
+            user.put("fName", fullName.getText().toString());
+
+            documentReference.set(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                @Override
+                public void onSuccess(Void Void) {
+                    Toast.makeText(ProfileActivity.this, "Your Profile was updated Successfully", Toast.LENGTH_SHORT).show();
+                    sendtoMain();
+                }
+             });
+        }
     }
 
 }
