b966116e8e5ac7bf411e2673b72d361d01712ac2
Icrum <hsals1234@gmail.com>
2021-01-15 22:48:51 +0000

    Chart working with time, version 2.0

--- app/src/main/java/com/example/dataserver/StatsActivity.java
+++ app/src/main/java/com/example/dataserver/StatsActivity.java
@@ -19,6 +19,8 @@ import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
 import com.google.firebase.database.ValueEventListener;
 import com.jjoe64.graphview.GraphView;
+import com.jjoe64.graphview.Viewport;
+import com.jjoe64.graphview.helper.DateAsXAxisLabelFormatter;
 import com.jjoe64.graphview.series.DataPoint;
 import com.jjoe64.graphview.series.LineGraphSeries;
 
@@ -34,10 +36,12 @@ import java.util.Map;
 
 public class StatsActivity extends AppCompatActivity {
     private String currentSensor;
-    private LineGraphSeries<DataPoint> series1;
+    private LineGraphSeries<DataPoint> series;
 
     DatabaseReference mRef;
     List<SensorValue> sensorValueList = new ArrayList<>(); // model
+    SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
+    GraphView graph;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,7 @@ public class StatsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_stats);
 
         final TextView mCurrentValue = (TextView) findViewById(R.id.textView_CurrentValue);
+        //final SimpleDateFormat sdf = new SimpleDateFormat("HH:mm");
 
         currentSensor = getIntent().getExtras().get("Sensor").toString();
         Toast.makeText(StatsActivity.this, currentSensor, Toast.LENGTH_SHORT).show();
@@ -56,17 +61,16 @@ public class StatsActivity extends AppCompatActivity {
         mRef = FirebaseDatabase.getInstance().getReference()
                 .child(currentSensor);
 
-        final GraphView graph = (GraphView) findViewById(R.id.graph);
-
-        // activate horizontal zooming and scrolling
-        graph.getViewport().setScalable(false);
+        graph = (GraphView) findViewById(R.id.graph);
         // activate horizontal scrolling
         graph.getViewport().setScrollable(true);
-        // activate horizontal and vertical zooming and scrolling
-        graph.getViewport().setScalableY(false);
-        // activate vertical scrolling
-        graph.getViewport().setScrollableY(false);
 
+        graph.getGridLabelRenderer().setNumHorizontalLabels(3); // only 4 because of the space
+        // as we use dates as labels, the human rounding to nice readable numbers
+        // is not necessary
+        graph.getGridLabelRenderer().setHumanRounding(false);
+
+        series = new LineGraphSeries();
 
         mRef.addValueEventListener(new ValueEventListener() {
             @Override
@@ -91,30 +95,46 @@ public class StatsActivity extends AppCompatActivity {
                     }
                 });
 
-                sensorValueList = sensorValueList.subList(sensorValueList.size()-12, sensorValueList.size());
-                mCurrentValue.setText("Current Value: " + sensorValueList.get(sensorValueList.size()-1).getValue());
+                sensorValueList = sensorValueList.subList(sensorValueList.size()-12,
+                        sensorValueList.size());
+                mCurrentValue.setText("Current Value: " + sensorValueList
+                        .get(sensorValueList.size()-1)
+                        .getValue());
 
-                Date x;
-                SimpleDateFormat sdf = new SimpleDateFormat("HH");
-                SimpleDateFormat sdf2 = new SimpleDateFormat("mm");
-                double y, x1, x2;
-                //String[] time= new String[12];
+                Date x = null, x1;
+                double y;
+                //get first date in sensorValueList
+                x1 = sensorValueList.get(0).getDate();
 
-                series1 = new LineGraphSeries<DataPoint>();
                 for (int i = 0; i < sensorValueList.size(); i++)
                 {
                     x = sensorValueList.get(i).getDate();
                     y = sensorValueList.get(i).getValue();
-                    x1 = Double.parseDouble(sdf.format(x.getTime()));
-                    x2 = Double.parseDouble(sdf2.format(x.getTime()));
-
-                    series1.appendData(new DataPoint(x1+(x2*0.016), y),
+                    series.appendData(new DataPoint(x.getTime(), y),
                             true,
-                            sensorValueList.size()+2);
-
+                            sensorValueList.size());
                 }
-                graph.addSeries(series1);
+                graph.addSeries(series);
 
+                // set date label formatter
+                graph.getGridLabelRenderer().setLabelFormatter(new DateAsXAxisLabelFormatter
+                        (StatsActivity.this)
+                {
+                    @Override
+                    public String formatLabel(double value, boolean isValueX) {
+                        if(isValueX){
+                            return sdf.format(new Date((long) value));
+                        }
+                        else{
+                            return super.formatLabel(value, isValueX);
+                        }
+                    }
+                });
+
+                // set manual x bounds to have nice steps
+                graph.getViewport().setMinX(x1.getTime());
+                graph.getViewport().setMaxX(x.getTime());
+                graph.getViewport().setXAxisBoundsManual(true);
             }
 
             @Override
